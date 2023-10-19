fa26ebb8f44117b6d3e704493baa9866740408e1
Ramon A. Rivera <ramonrivera550@gmail.com>
2023-10-18 22:24:52 -0400

    Example JS program

---
+++ app.js
@@ -0,0 +1,29 @@
+const rect = {
+    perimeter: (x, y) => 2 * (x + y),
+    area: (x, y) => x * y
+};
+
+function solveRect(l, w) {
+    console.log(`Solving for rectangle with dimensions: ${l}, ${w}`);
+
+    if (l <= 0 || w <= 0) {
+        console.log(`Rectangle dimensions must be greater than zero. Received: ${l}, ${w}`);
+    } else {
+        console.log(`Area of rectangle: ${rect.area(l, w)}`);
+        console.log(`Perimeter of rectangle: ${rect.perimeter(l, w)}`);
+    }
+}
+
+solveRect(2, 4);
+solveRect(3, 5);
+solveRect(0, 5);
+solveRect(5, -3);
+
+/* Create a variable named rect, short for rectangle.
+It will take two methods. Methods are always written inside curly braces.
+First method is the perimeter and it takes two values. The perimeter is the sum of all four sides.
+The area method also takes two values. Then return the product of those two values by multiplying them together. 
+L and W stand for Length and Width. 
+Use templeta literal, meaning use backticks, not quotation marks (Line7). 
+
+*/
